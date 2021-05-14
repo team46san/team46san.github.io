3c9a260af8fc64a4e04716f965c57487781dd1c8
team46san <78849250+team46san@users.noreply.github.com>
2021-05-14 19:26:25 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAaVY-Bx-VeZ-pIXWtChCOe2URgxNh9GfU",
+  authDomain: "jmschatbot.firebaseapp.com",
+  projectId: "jmschatbot",
+  storageBucket: "jmschatbot.appspot.com",
+  messagingSenderId: "726435560597",
+  appId: "1:726435560597:web:246170c0ac585c14a92f9c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
